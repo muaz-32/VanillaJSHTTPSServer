ecfef5ce3b6db893c874520259cdd94b9d93e8a7
muaz-32 <96006730+muaz-32@users.noreply.github.com>
2024-06-10 17:02:18 +0600

    Added a post request to store the data temporarily.

--- server.js
+++ server.js
@@ -6,11 +6,23 @@ const options = {
     cert: fs.readFileSync('cert.pem')
 };
 
+let inputs = [];
+
 https.createServer(options, (req, res) => {
     if (req.method === 'GET' && req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         const readStream = fs.createReadStream('index.html');
         readStream.pipe(res);
+    } else if (req.method === 'POST' && req.url === '/submit') {
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            inputs.push(body);
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Data received: ' + inputs);
+        });
     }
 }).listen(8000, () => {
     console.log('Server running at https://localhost:8000/');
